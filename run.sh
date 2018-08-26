69f1fe7106522b949f5ea2fb6c7afdf03579b4fe
SukkaW <isukkaw@gmail.com>
2018-08-26 12:26:53 +0800

    Update template and build script

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
 mkdir tmp -p
 touch tmp/main.html
+
 curl -s "https://api.imjad.cn/pixiv/v2/?type=rank&page=1" > tmp/pixiv.json
+
+chmod +x parse.sh
+./parse.sh
+
+cp -rf src/index.html tmp/index.html
+
+pixivHTML=$(cat tmp/main.html)
+
+sed -i "s|{{Main}}|$pixivHTML|g" tmp/index.html
