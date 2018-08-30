58620ba3edd8a12799d05963751528c34eb964ff
SukkaW <isukkaw@gmail.com>
2018-08-30 13:59:34 +0800

    Fix template

--- parse.sh
+++ parse.sh
@@ -2,11 +2,11 @@
 for ((i=0;i<30;i++))
 do
     id=$(jq -r '.illusts['$i'].id' tmp/pixiv.json)
-    title=$(jq -r '.illusts['$i'].title' tmp/pixiv.json | sed "s~<~&lt;~g" | sed "s~<~&gt;~g")
+    title=$(jq -r '.illusts['$i'].title' tmp/pixiv.json | sed 's~<~&lt;~g' | sed 's~>~&gt;~g')
     img_medium=$(jq -r '.illusts['$i'].image_urls.medium' tmp/pixiv.json)
     img_large=$(jq -r '.illusts['$i'].image_urls.large' tmp/pixiv.json)
     img_original=$(echo ${img_large} | sed "s~/c/600x1200_90/img-master~/img-original~g" | sed "s~_master1200~~g")
-    author=$(jq -r '.illusts['$i'].user.name' tmp/pixiv.json | sed "s~<~&lt;~g" | sed "s~<~&gt;~g")
+    author=$(jq -r '.illusts['$i'].user.name' tmp/pixiv.json | sed 's~<~&lt;~g' | sed 's~>~&gt;~g')
 
     echo '<article class="thumb">' >> tmp/main.html
     echo '<a href="'${img_original}'" class="image lazyload" data-src="'${img_medium}'">' >> tmp/main.html
